17e9fa77952587cbca9558e160d05a7c048a453d
Luis Ferreira <luis.ferreira@freiheit.com>
2024-01-27 13:52:33 +0000

    A more dynamic loading of animals and screen constraints

--- main.py
+++ main.py
@@ -62,20 +62,28 @@ class Recorder:
 
 
 class Animal:
-    def __init__(self, name: str, surface: pg.Surface, background_color: pg.Color, foreground_color: pg.Color):
+    def __init__(self, name: str, asset_src: str, background_color: pg.Color, foreground_color: pg.Color):
         self.name = name
-        self.surface = surface
-        self.surface_w, self.surface_h = surface.get_size()
+        self.asset_src = asset_src
+        self.surface = None
+        self.surface_w, self.surface_h = 0, 0
         self.background_color = background_color
         self.foreground_color = foreground_color
 
+    def load_surface(self, screen_constraints: Tuple[int, int]):
+        self.surface = pg.image.load(self.asset_src)
+        self.surface_w, self.surface_h = self.surface.get_size()
+        if self.surface_w != screen_constraints[0] * 2 / 4 or self.surface_h != screen_constraints[1] * 2 / 4:
+            self.surface = pg.transform.scale(self.surface, (screen_constraints[0] * 2 / 4, screen_constraints[1] * 2 / 4))
+
 
 class GameState:
-    def __init__(self, recorder: Recorder, animal_assets_src: List[str], animal_characteristics: List[Tuple[str, pg.Color, pg.Color]]):
+    def __init__(self, recorder: Recorder, animals: List[Animal]):
+        self.screen_constraints = SCREEN_SIZE
+        self.screen_constraints_w, self.screen_constraints_h = SCREEN_SIZE
+
         self.recorder = recorder
-        self.animal_assets_src = animal_assets_src
-        # TODO: this does not feel right to be a tuple
-        self.animal_characteristics = animal_characteristics
+        self.animals = animals
 
         self.current_page = PAGE_SHOW_THE_ANIMAL_SAYS
         self.current_animal = self.next_animal()
@@ -86,46 +94,43 @@ class GameState:
         self.current_evaluation = 0
 
     def next_animal(self) -> Animal:
-        next_animal_asset, next_animal_characteristics = self.animal_assets_src.pop(), self.animal_characteristics.pop()
-        next_animal_surface = pg.image.load(next_animal_asset)
-        next_animal = Animal(next_animal_characteristics[0], next_animal_surface, next_animal_characteristics[1], next_animal_characteristics[2])
-        if next_animal.surface_w != SCREEN_WIDTH * 2 / 4 or next_animal.surface_h != SCREEN_HEIGHT * 2 / 4:
-            next_animal.surface = pg.transform.scale(next_animal.surface, (SCREEN_WIDTH * 2 / 4, SCREEN_HEIGHT * 2 / 4))
-        self.current_animal = next_animal
-        return next_animal
+        # TODO: we probably want some sort of randomness here
+        self.current_animal = self.animals.pop()
+        self.current_animal.load_surface(SCREEN_SIZE)
+        return self.current_animal
 
 
 def draw_page_show_the_animal_says(screen: pg.Surface, global_game_state: GameState):
     animal = global_game_state.current_animal
     screen.fill(animal.background_color)
-    screen.blit(animal.surface, (SCREEN_WIDTH * 1 / 4, SCREEN_HEIGHT * 1 / 4))
+    screen.blit(animal.surface, (global_game_state.screen_constraints_w * 1 / 4, global_game_state.screen_constraints_h * 1 / 4))
 
     record_text = font.render(f"The {animal.name} says...", True, FONT_COLOR)
-    record_rect = record_text.get_rect(center=(SCREEN_WIDTH / 2, SCREEN_HEIGHT - 100))
+    record_rect = record_text.get_rect(center=(global_game_state.screen_constraints_w / 2, global_game_state.screen_constraints_h - 100))
     screen.blit(record_text, record_rect)
 
 
 def draw_page_show_the_animal_recording(screen: pg.Surface, global_game_state: GameState):
     animal = global_game_state.current_animal
     screen.fill(animal.background_color)
-    screen.blit(animal.surface, (SCREEN_WIDTH * 1 / 4, SCREEN_HEIGHT * 1 / 4))
+    screen.blit(animal.surface, (global_game_state.screen_constraints_w * 1 / 4, global_game_state.screen_constraints_h * 1 / 4))
 
     record_text = font.render(f"Recording your {animal.name} sound", True, FONT_COLOR)
-    record_rect = record_text.get_rect(center=(SCREEN_WIDTH / 2, SCREEN_HEIGHT - 100))
+    record_rect = record_text.get_rect(center=(global_game_state.screen_constraints_w / 2, global_game_state.screen_constraints_h - 100))
     screen.blit(record_text, record_rect)
 
 
 def draw_page_show_the_animal_score(screen: pg.Surface, global_game_state: GameState):
     animal = global_game_state.current_animal
     screen.fill(animal.background_color)
-    screen.blit(animal.surface, (SCREEN_WIDTH * 1 / 4, SCREEN_HEIGHT * 1 / 4))
+    screen.blit(animal.surface, (global_game_state.screen_constraints_w * 1 / 4, global_game_state.screen_constraints_h * 1 / 4))
 
     score_text = font.render(f"Your similarity with the {animal.name} is: {global_game_state.current_evaluation}%", True, FONT_COLOR)
-    score_rect = score_text.get_rect(center=(SCREEN_WIDTH / 2, SCREEN_HEIGHT - 100))
+    score_rect = score_text.get_rect(center=(global_game_state.screen_constraints_w / 2, global_game_state.screen_constraints_h - 100))
     screen.blit(score_text, score_rect)
 
     feedback_text = font.render(f"You {'rock!' if global_game_state.current_evaluation >= 60 else 'suck...'}", True, FONT_COLOR)
-    feedback_rect = feedback_text.get_rect(center=(SCREEN_WIDTH / 2, SCREEN_HEIGHT - 50))
+    feedback_rect = feedback_text.get_rect(center=(global_game_state.screen_constraints_w / 2, global_game_state.screen_constraints_h - 50))
     screen.blit(feedback_text, feedback_rect)
 
 
@@ -133,12 +138,13 @@ def handle_event(event: pg.event.Event, global_game_state: GameState) -> GameSta
     if event.type == pg.KEYDOWN and event.key == pg.K_SPACE and global_game_state.current_page == PAGE_SHOW_THE_ANIMAL_SAYS:
         global_game_state.recorder.start_recording()
         global_game_state.current_page = PAGE_SHOW_THE_ANIMAL_RECORDING
-    if event.type == pg.KEYDOWN and event.key == pg.K_SPACE and global_game_state.current_page == PAGE_SHOW_THE_ANIMAL_RECORDING:
+    elif event.type == pg.KEYDOWN and event.key == pg.K_SPACE and global_game_state.current_page == PAGE_SHOW_THE_ANIMAL_RECORDING:
         global_game_state.recorder.stop_recording()
         # TODO: calculate similarity
         global_game_state.current_evaluation = 60
         global_game_state.current_page = PAGE_SHOW_THE_ANIMAL_SCORE
-    if event.type == pg.KEYDOWN and event.key == pg.K_SPACE and global_game_state.current_page == PAGE_SHOW_THE_ANIMAL_SCORE:
+    elif event.type == pg.KEYDOWN and event.key == pg.K_SPACE and global_game_state.current_page == PAGE_SHOW_THE_ANIMAL_SCORE:
+        # TODO: load next animal!
         global_game_state.current_page = PAGE_SHOW_THE_ANIMAL_SAYS
     return global_game_state
 
@@ -151,9 +157,8 @@ draw_page_map = {
 
 
 def main_game_loop():
-    # TODO: dynamically load assets and unload them as we move between animals?
-    # TODO: have the colors also in some sort of config file
-    global_game_state = GameState(Recorder(), ["tmp/cow.webp"], [("cow", BACKGROUND_COLOR, FONT_COLOR)])
+    # TODO: load animal list from a json config
+    global_game_state = GameState(Recorder(), [Animal("cow", "tmp/cow.webp", BACKGROUND_COLOR, FONT_COLOR)])
 
     while True:
         # process events
